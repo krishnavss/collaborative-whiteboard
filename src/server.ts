c615de65697d44001f1c5893b73e5ca97efc4e17
krishnavss <krishnavss@gmail.com>
2025-08-03 23:47:37 -0700

    feat: setup initial Node.js server with TypeScript

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Whiteboard Backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
